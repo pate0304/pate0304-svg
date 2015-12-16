f22347d7d72cdf0c6b9c8ea0e0d348575f6b8fcd
Jay Patel <pate0304@algonquinlive.com>
2015-12-16 13:43:50 -0500

    svg/ animations

---
+++ script.js
@@ -0,0 +1,32 @@
+
+ function micOff(){
+$("#mic").css("background","red");
+	}
+   
+ function micOn(){
+$("#mic").css("background","Green");
+	}
+   
+function micDisconnect(){
+	$("#mic").css("display","none");
+	}
+function micConnect(){
+	$("#mic").css("display","block");
+	}
+
+
+
+ function hpOff(){
+$("#hp").css("background","red");
+	}
+   
+ function hpOn(){
+$("#hp").css("background","Green");
+	}
+   
+function hpDisconnect(){
+	$("#hp").css("display","none");
+	}
+function hpConnect(){
+	$("#hp").css("display","block");
+	}
